842080752d3939f310b1decea48e8d815917315f
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-04-28 01:14:08 +0300

    modified:   xray-generate-config.py

--- xray-generate-config.py
+++ xray-generate-config.py
@@ -5,7 +5,7 @@ import sys
 # -----------------------------
 # НАСТРОЙКИ
 # -----------------------------
-DOMAIN_WHITELIST = []
+DOMAIN_WHITELIST = ["auto2.freenternet.top", "auto.freenternet.top"]
 
 
 # -----------------------------
