88cd65d8f28d193ed029eb793a479a1ad9be432a
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-05-01 14:20:44 +0300

    modified:   xray-generate-config.py

--- xray-generate-config.py
+++ xray-generate-config.py
@@ -60,7 +60,7 @@ def choose_best_server(servers):
 def base_config():
     return {
         "log": {
-            "loglevel": "warning",
+            "loglevel": "debug",
             "access": "/tmp/log/xray-access.log",
             "error": "/tmp/log/xray-error.log"
         },
