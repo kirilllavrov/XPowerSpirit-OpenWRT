e9def18f801a80b38236b615968276d0a4b49446
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-05-03 00:05:42 +0300

    modified:   setup-guest-network.sh

--- setup-guest-network.sh
+++ setup-guest-network.sh
@@ -140,7 +140,7 @@ uci -q delete firewall.${GUEST_NET}_rtr
 uci set firewall.${GUEST_NET}_rtr="rule"
 uci set firewall.${GUEST_NET}_rtr.name="Block-${GUEST_NET}-to-router"
 uci set firewall.${GUEST_NET}_rtr.src="$GUEST_NET"
-uci set firewall.${GUEST_NET}_rtr.dest_ip="$MAIN_LAN_IP/32"
+uci set firewall.${GUEST_NET}_rtr.dest="lan"
 uci set firewall.${GUEST_NET}_rtr.target="REJECT"
 
 uci -q delete firewall.${GUEST_NET}_dns
