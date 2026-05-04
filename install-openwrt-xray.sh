8b74773a864ed2da4fdf2f6ba153748821aea215
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-05-04 13:58:47 +0300

    modified:   install-openwrt-xray.sh

--- install-openwrt-xray.sh
+++ install-openwrt-xray.sh
@@ -313,11 +313,11 @@ update_geo() {
 
 # Вызовы
 update_geo \
-	"https://cdn.jsdelivr.net/gh/kirilllavrov/geoip-builder@release/geoip.dat" \
+	"https://raw.githubusercontent.com/kirilllavrov/geoip-builder/release/geoip.dat" \
 	"$GEO_DIR/geoip.dat"
 
 update_geo \
-	"https://cdn.jsdelivr.net/gh/kirilllavrov/geosite-builder@release/geosite.dat" \
+	"https://raw.githubusercontent.com/kirilllavrov/geosite-builder/release/geosite.dat" \
 	"$GEO_DIR/geosite.dat"
 
 HWID="$(hexdump -n 16 -v -e '/1 "%02x"' /dev/urandom)"
