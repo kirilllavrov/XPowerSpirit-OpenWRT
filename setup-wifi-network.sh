45e76ac8cd375a35691d48fcf3b646bab9a7b0bb
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-05-10 11:49:04 +0300

    modified:   setup-wifi-network.sh

--- setup-wifi-network.sh
+++ setup-wifi-network.sh
@@ -73,7 +73,9 @@ for RADIO in $(uci show wireless | sed -n 's/^\(wireless\.\([^=]*\)\)=wifi-devic
 done
 
 # 5GHz — HE160
+# HE160 может не поддерживаться всеми устройствами, HE80 — более совместимый вариант
 #uci -q set wireless.radio1.htmode='HE160'
+uci -q set wireless.radio1.htmode='HE80' 
 
 # Time Advertisement
 uci -q set wireless.radio0.time_advertisement='2'
