8581e6b526f703f405c016d0b203937c83c32253
Kirill_Lavrov <kirill.lavrov@checloud.ru>
2026-05-11 15:28:55 +0300

    modified:   install-openwrt-xray.sh

--- install-openwrt-xray.sh
+++ install-openwrt-xray.sh
@@ -425,25 +425,34 @@ echo "[+] Все скрипты загружены и готовы к испол
 # =============================================
 echo "7. Настраиваем DNS (dnsmasq + https-dns-proxy)..."
 
+# Отключаем принудительный DNS для LAN (не ломаем Android Private DNS)
+uci set https-dns-proxy.config.force_dns='0'
+
+# Настройка dnsmasq: strict order → NextDNS, Cloudflare, Yandex DoH, Google, Yandex UDP
 uci set dhcp.@dnsmasq[0].noresolv='1'
 uci set dhcp.@dnsmasq[0].strictorder='1'
 uci -q delete dhcp.@dnsmasq[0].server
-uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5053'
-uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5054'
 uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5055'
+uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5053'
+uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5056'
+uci add_list dhcp.@dnsmasq[0].server='127.0.0.1#5054'
 uci add_list dhcp.@dnsmasq[0].server='77.88.8.8'
 uci commit dhcp
 
-# Добавляем к дефолту NextDNS и его bootstrap, а также включаем HTTP/3 и force IPv4 для всех резолверов
+# Добавляем NextDNS
 uci add https-dns-proxy https-dns-proxy
 uci set https-dns-proxy.@https-dns-proxy[-1].listen_addr='127.0.0.1'
 uci set https-dns-proxy.@https-dns-proxy[-1].listen_port='5055'
 uci set https-dns-proxy.@https-dns-proxy[-1].resolver_url='https://dns.nextdns.io'
 uci set https-dns-proxy.@https-dns-proxy[-1].bootstrap_dns='45.90.28.0,45.90.30.0'
-uci set https-dns-proxy.@https-dns-proxy[0].tcp_client_limit='50'
-uci set https-dns-proxy.@https-dns-proxy[0].force_ipv4='1'
-uci set https-dns-proxy.@https-dns-proxy[1].force_ipv4='1'
-uci set https-dns-proxy.@https-dns-proxy[2].force_ipv4='1'
+
+# Добавляем Yandex DoH
+uci add https-dns-proxy https-dns-proxy
+uci set https-dns-proxy.@https-dns-proxy[-1].listen_addr='127.0.0.1'
+uci set https-dns-proxy.@https-dns-proxy[-1].listen_port='5056'
+uci set https-dns-proxy.@https-dns-proxy[-1].resolver_url='https://dns.yandex.ru/dns-query'
+uci set https-dns-proxy.@https-dns-proxy[-1].bootstrap_dns='77.88.8.8,77.88.8.1'
+
 uci commit https-dns-proxy
 
 echo "[+] DNS настроен"
